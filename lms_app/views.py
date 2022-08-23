912b3d4abb608f3bf28c8f85e41d7ffafd3c7e8e
Hany Sameh <hanysameh@Hanys-MacBook-Pro.local>
2022-08-23 21:35:02 +0200

    get data from databasr

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    context ={
+        'books':Book.objects.all(),
+        'categores':Category.objects.all(), 
+    }
+    return render(request, 'pages/index.html',context=context)
 
 
 def books(request):
     return render(request, 'pages/books.html')
+
+
