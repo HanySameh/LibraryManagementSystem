dc7a388c998aaa7884b2456c020dd197afd501ce
Hany Sameh <hanysameh@Hanys-MacBook-Pro.local>
2022-08-25 20:44:35 +0200

    add forms and add books and category to DB

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
 from .models import *
+from .forms import *
+
 # Create your views here.
 
 
 def index(request):
-    context ={
-        'books':Book.objects.all(),
-        'categores':Category.objects.all(), 
+    if request.method == 'POST':
+        add_book = BookForms(request.POST, request.FILES)
+        if add_book.is_valid():
+            add_book.save()
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
+    context = {
+        'books': Book.objects.all(),
+        'categores': Category.objects.all(),
+        'form': BookForms(),
+        'cat_form': CategoryForm()
     }
-    return render(request, 'pages/index.html',context)
+    return render(request, 'pages/index.html', context)
 
 
 def books(request):
-    context ={
-        'books':Book.objects.all(),
-        'categores':Category.objects.all(), 
+    context = {
+        'books': Book.objects.all(),
+        'categores': Category.objects.all(),
+        'cat_form': CategoryForm()
+
     }
-    return render(request, 'pages/books.html',context)
-
-
+    return render(request, 'pages/books.html', context)
